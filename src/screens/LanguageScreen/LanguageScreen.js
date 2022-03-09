5134ccb68626f72a1cf137b06a6ce8f37b79a50f
peiko.kruta.m <kurtamykhailo@gmail.com>
2022-03-09 10:12:44 +0200

    Added push notification for Android

--- src/screens/LanguageScreen/LanguageScreen.js
+++ src/screens/LanguageScreen/LanguageScreen.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 
 import {useDispatch, useSelector} from 'react-redux';
 import {completed} from '../../store/features/onboarding/onboardingSlice';
+import messaging from '@react-native-firebase/messaging';
 
 import styles from './styles';
 
 const LanguageScreen = ({navigation}) => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   const dispatch = useDispatch();
   const count = useSelector(state => state);
   console.log('HERE');
@@ -14,6 +23,7 @@ const LanguageScreen = ({navigation}) => {
   const onPressHandler = () => {
     navigation.navigate('WelcomeScreen');
   };
+
   return (
     <View style={styles.continaer}>
       <Text>LanguageScreen</Text>
