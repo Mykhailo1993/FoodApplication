eca15c515a43c9baad64a43d72d538920007e1cc
peiko.kruta.m <kurtamykhailo@gmail.com>
2022-06-20 19:55:20 +0300

    Added push notification for android

--- src/screens/LanguageScreen/LanguageScreen.js
+++ src/screens/LanguageScreen/LanguageScreen.js
@@ -1,16 +1,37 @@
 import React, {useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, Platform} from 'react-native';
 
 import {useDispatch, useSelector} from 'react-redux';
 import {completed} from '../../store/features/onboarding/onboardingSlice';
 import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
 import styles from './styles';
 
 const LanguageScreen = ({navigation}) => {
   useEffect(() => {
+    PushNotification.createChannel(
+      {
+        channelId: '1000',
+        channelName: 'My',
+        playSound: true,
+      },
+      a => {
+        console.log(a);
+      },
+    );
+  }, []);
+  useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      if (Platform.OS === 'android') {
+        PushNotification.localNotification({
+          title: remoteMessage.title,
+          message: remoteMessage.title,
+          channelId: '1000',
+          playSound: true,
+        });
+      }
     });
 
     return unsubscribe;
